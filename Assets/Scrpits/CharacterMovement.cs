6adb723883e38fb28364613cdebda264c15ab1a3
Furkan <143404963+SnooiPie@users.noreply.github.com>
2025-09-04 03:20:17 +0300

    tama

--- Assets/Scrpits/CharacterMovement.cs
+++ Assets/Scrpits/CharacterMovement.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+
     void Start()
     {
-        
+        // Eğer Rigidbody varsa etkisizleştirelim
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.useGravity = false;
+            rb.isKinematic = true;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // A/D tuşları = Horizontal input
+        float move = Input.GetAxis("Horizontal"); 
+
+        // Ama hareketi X yerine Z eksenine uygula
+        Vector3 movement = new Vector3(0, 0, move) * speed * Time.deltaTime;
+
+        transform.Translate(movement, Space.World);
     }
 }
